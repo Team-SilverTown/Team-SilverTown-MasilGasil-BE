6debe6ddacf00e82c4b3d94f93d02e223b7d8bfa
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-02-12 20:34:26 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/team/silvertown/masil/MasilApplication.java
@@ -0,0 +1,13 @@
+package team.silvertown.masil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/silvertown/masil/MasilApplicationTests.java
@@ -0,0 +1,13 @@
+package team.silvertown.masil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
