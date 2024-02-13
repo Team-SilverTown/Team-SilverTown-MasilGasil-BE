c3009a874d757f3de6ee236603a83c037b848284
Jay <injaesong0@gmail.com>
2024-02-14 02:27:43 +0900

    chore: 테스트 h2 DB 설정 추가

--- src/test/java/team/silvertown/masil/MasilApplicationTests.java
+++ src/test/java/team/silvertown/masil/MasilApplicationTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import team.silvertown.masil.user.domain.User;
 
 @SpringBootTest
 @DisplayNameGeneration(ReplaceUnderscores.class)
