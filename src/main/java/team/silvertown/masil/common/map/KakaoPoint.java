3e5aa25d3f68cd623627acfab9ab5fb519006b6a
Jay <injaesong0@gmail.com>
2024-02-16 18:13:23 +0900

    feat: 카카오 포인트 추가

---
+++ src/main/java/team/silvertown/masil/common/map/KakaoPoint.java
@@ -0,0 +1,24 @@
+package team.silvertown.masil.common.map;
+
+import lombok.Getter;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
+
+@Getter
+public record KakaoPoint(double lat, double lng) {
+
+    public Point toPoint(WKTReader wktReader) {
+        try {
+            return (Point) wktReader.read("POINT(" + this.toRawString() + ")");
+        } catch (ParseException e) {
+            // Exception 작업 후 수정 예정
+            throw new IllegalArgumentException("Wrong point format");
+        }
+    }
+
+    public String toRawString() {
+        return this.lat + " " + this.lng;
+    }
+
+}
