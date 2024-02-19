465c314f0588abff5c6082d7d977871b6b9b11ab
Jay <injaesong0@gmail.com>
2024-02-19 19:07:52 +0900

    feat: 주소 클래스 검증 추가

--- src/main/java/team/silvertown/masil/common/map/Address.java
+++ src/main/java/team/silvertown/masil/common/map/Address.java
@@ -2,9 +2,14 @@ package team.silvertown.masil.common.map;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.silvertown.masil.common.exception.BadRequestException;
+import team.silvertown.masil.common.validator.Validator;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Address {
 
@@ -16,5 +21,20 @@ public class Address {
 
     @Column(name = "address_depth3", length = 20, nullable = false)
     private String depth3;
+
+    public Address(String depth1, String depth2, String depth3, String depth4) {
+        Validator.notBlank(depth1, () -> new BadRequestException(MapErrorCode.BLANK_DEPTH1));
+        Validator.notNull(depth2, () -> new BadRequestException(MapErrorCode.NULL_DEPTH2));
+        Validator.notBlank(depth3, () -> new BadRequestException(MapErrorCode.BLANK_DEPTH3));
+
+        this.depth1 = depth1;
+        this.depth2 = depth2;
+
+        if (!depth4.isBlank()) {
+            depth3 += " " + depth4;
+        }
+
+        this.depth3 = depth3;
+    }
 
 }
