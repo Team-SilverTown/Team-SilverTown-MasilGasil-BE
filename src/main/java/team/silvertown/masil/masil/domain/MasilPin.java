a4736ddb6698cad67f4a84d2f02910f074a1fd42
Jay <injaesong0@gmail.com>
2024-02-20 14:54:28 +0900

    feat: 마실 핀 도메인 검증 추가

--- src/main/java/team/silvertown/masil/masil/domain/MasilPin.java
+++ src/main/java/team/silvertown/masil/masil/domain/MasilPin.java
@@ -10,18 +10,17 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.locationtech.jts.geom.Point;
 import team.silvertown.masil.common.BaseEntity;
+import team.silvertown.masil.masil.exception.MasilErrorCode;
+import team.silvertown.masil.masil.validator.MasilValidator;
 
 @Entity
 @Table(name = "masil_pins")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
 @Getter
 public class MasilPin extends BaseEntity {
 
@@ -44,5 +43,17 @@ public class MasilPin extends BaseEntity {
 
     @Column(name = "thumbnail_url", length = 1024)
     private String thumbnailUrl;
+
+    @Builder
+    private MasilPin(Masil masil, Long userId, Point point, String content, String thumbnailUrl) {
+        MasilValidator.notNull(masil, MasilErrorCode.NULL_MASIL);
+        MasilValidator.validateUrl(thumbnailUrl);
+
+        this.masil = masil;
+        this.userId = userId;
+        this.point = point;
+        this.content = content;
+        this.thumbnailUrl = thumbnailUrl;
+    }
 
 }
