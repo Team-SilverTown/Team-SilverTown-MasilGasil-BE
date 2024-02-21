3f752a2d4bb5938c0c99587bf2a6a876661d5b21
Jay <injaesong0@gmail.com>
2024-02-21 18:38:29 +0900

    feat: 임시의 유저 레퍼지터리 추가

---
+++ src/main/java/team/silvertown/masil/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package team.silvertown.masil.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.silvertown.masil.user.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
