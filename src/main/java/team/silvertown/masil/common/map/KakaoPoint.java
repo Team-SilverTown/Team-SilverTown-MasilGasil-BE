4e0f0d6f1807024dd1fa244f52afde8e1d403820
Jay <injaesong0@gmail.com>
2024-02-23 02:18:08 +0900

    feat: 포인트로부터 카카오 포인트 생성 메서드 추가

--- src/main/java/team/silvertown/masil/common/map/KakaoPoint.java
+++ src/main/java/team/silvertown/masil/common/map/KakaoPoint.java
@@ -1,9 +1,15 @@
 package team.silvertown.masil.common.map;
 
+import org.locationtech.jts.geom.Point;
+
 public record KakaoPoint(double lat, double lng) {
 
     public String toRawString() {
         return this.lat + " " + this.lng;
+    }
+
+    public static KakaoPoint from(Point point) {
+        return new KakaoPoint(point.getX(), point.getY());
     }
 
 }
