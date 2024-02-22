3cfec1bee63cebd20edf1c25e1de53b4ce7f39e6
shoeone96 <dlwnddnjs96@gmail.com>
2024-02-22 15:10:36 +0900

    feat: jwt 토큰을 정상적으로 반환하는 로직 추가

---
+++ src/test/java/team/silvertown/masil/config/jwt/JwtTokenProviderTest.java
@@ -0,0 +1,36 @@
+package team.silvertown.masil.config.jwt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class JwtTokenProviderTest {
+
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
+    @Test
+    public void 정상적으로_토큰_생성이_가능하다() throws Exception {
+        //given
+        Long userId = 1L;
+
+        //when
+        String token = jwtTokenProvider.createToken(userId);
+
+        //then
+        assertThat(jwtTokenProvider.validateToken(token)).isTrue();
+        assertThat(jwtTokenProvider.getAuthentication(token).getPrincipal() instanceof Long).isTrue();
+        Long decodedUserId = (Long) jwtTokenProvider.getAuthentication(token)
+            .getPrincipal();
+        assertThat(userId).isEqualTo(decodedUserId);
+
+    }
+
+}
