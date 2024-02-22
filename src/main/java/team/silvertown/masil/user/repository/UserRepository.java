aed91075a34e0331789290d796f9ef0415072f25
shoeone96 <dlwnddnjs96@gmail.com>
2024-02-22 15:07:02 +0900

    feat: user 조회 및 유저 권한 목록 조회 기능 추가

---
+++ src/main/java/team/silvertown/masil/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package team.silvertown.masil.user.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.silvertown.masil.user.domain.Provider;
+import team.silvertown.masil.user.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderAndSocialId(Provider provider, String socialId);
+}
