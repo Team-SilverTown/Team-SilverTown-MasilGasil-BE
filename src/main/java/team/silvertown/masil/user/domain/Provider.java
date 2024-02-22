c81df2c613a04aff27b2e01b953a08dbd2571ec8
shoeone96 <dlwnddnjs96@gmail.com>
2024-02-22 15:09:18 +0900

    feat: 소셜 로그인 기능 밴더사 구분하는 로직 추가

--- src/main/java/team/silvertown/masil/user/domain/Provider.java
+++ src/main/java/team/silvertown/masil/user/domain/Provider.java
@@ -1,5 +1,27 @@
 package team.silvertown.masil.user.domain;
 
+import lombok.Getter;
+import team.silvertown.masil.user.exception.InvalidAuthenticationException;
+import team.silvertown.masil.user.exception.UserErrorCode;
+
+@Getter
 public enum Provider {
-    KAKAO
+    KAKAO("kakao");
+
+    private final String value;
+
+    Provider(String value) {
+        this.value = value;
+    }
+
+    public static Provider returnProvider(String value) {
+        Provider[] values = Provider.values();
+        for (Provider provider : values) {
+            if (provider.getValue()
+                .equals(value)) {
+                return provider;
+            }
+        }
+        throw new InvalidAuthenticationException(UserErrorCode.INVALID_PROVIDER);
+    }
 }
