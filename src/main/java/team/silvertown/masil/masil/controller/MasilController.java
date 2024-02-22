0518a49ce022ffff6b3b6039fc2204389306021c
Jay <injaesong0@gmail.com>
2024-02-23 02:21:30 +0900

    feat: 마실 단일 조회 API 엔드포인트 추가

--- src/main/java/team/silvertown/masil/masil/controller/MasilController.java
+++ src/main/java/team/silvertown/masil/masil/controller/MasilController.java
@@ -3,11 +3,14 @@ package team.silvertown.masil.masil.controller;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import team.silvertown.masil.masil.dto.CreateRequest;
 import team.silvertown.masil.masil.dto.CreateResponse;
+import team.silvertown.masil.masil.dto.MasilResponse;
 import team.silvertown.masil.masil.service.MasilService;
 
 @RestController
@@ -27,6 +30,16 @@ public class MasilController {
 
         return ResponseEntity.created(uri)
             .body(createResponse);
+    }
+
+    @GetMapping("/api/v1/masils/{id}")
+    public ResponseEntity<MasilResponse> getById(
+        @PathVariable
+        Long id
+    ) {
+        MasilResponse response = masilService.getById(1L, id);
+
+        return ResponseEntity.ok(response);
     }
 
 }
