d544fe7cd66cad3ae2ca5df7bca7a7ca070fe366
shoeone96 <dlwnddnjs96@gmail.com>
2024-02-22 15:10:14 +0900

    test: provider 반환 테스트 로직 추가

---
+++ src/test/java/team/silvertown/masil/user/domain/ProviderTest.java
@@ -0,0 +1,39 @@
+package team.silvertown.masil.user.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import team.silvertown.masil.user.exception.InvalidAuthenticationException;
+import team.silvertown.masil.user.exception.UserErrorCode;
+
+class ProviderTest {
+    
+    @Test
+    public void provider_정상_생성_테스트() throws Exception {
+        //given
+        String normalProvider = "kakao";
+
+        //when
+        Provider provider = Provider.returnProvider(normalProvider);
+        
+        //then
+        assertThat(provider.name()).isEqualTo("KAKAO");
+        assertThat(provider.getValue()).isEqualTo("kakao");
+    
+    }
+
+    @Test
+    public void 유효하지_않은_provider_예외호출_테스트() throws Exception {
+        //given, when
+        String invalidProvider = "bug";
+
+        //then
+        Assertions.assertThatThrownBy(() ->Provider.returnProvider(invalidProvider))
+            .isInstanceOf(InvalidAuthenticationException.class)
+            .hasMessage(UserErrorCode.INVALID_PROVIDER.getMessage());
+
+    }
+
+}
