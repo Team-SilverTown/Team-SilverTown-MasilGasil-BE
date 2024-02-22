b2e7079174b118cf01600954b77f2057b268aec2
Jay <injaesong0@gmail.com>
2024-02-23 02:20:23 +0900

    feat: 레퍼지터리에 마실로 마실 핀 조회 메서드 추가

--- src/main/java/team/silvertown/masil/masil/repository/MasilPinRepository.java
+++ src/main/java/team/silvertown/masil/masil/repository/MasilPinRepository.java
@@ -1,8 +1,12 @@
 package team.silvertown.masil.masil.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.silvertown.masil.masil.domain.Masil;
 import team.silvertown.masil.masil.domain.MasilPin;
 
 public interface MasilPinRepository extends JpaRepository<MasilPin, Long> {
+
+    List<MasilPin> findAllByMasil(Masil masil);
 
 }
