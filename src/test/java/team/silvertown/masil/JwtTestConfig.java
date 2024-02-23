f2f617889a7d628f92460efdcf3a332168fa2e0f
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-02-24 00:18:18 +0900

    refactor: test-yaml 파일에서 secret 정보 삭제 후 config 파일로 설정

---
+++ src/test/java/team/silvertown/masil/JwtTestConfig.java
@@ -0,0 +1,21 @@
+package team.silvertown.masil;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import team.silvertown.masil.config.jwt.JwtProperties;
+
+@Configuration
+public class JwtTestConfig {
+
+    private static final String JWT_ISSUER = "test issuer";
+    private static final String BASE64_SECRET = "testSecrettestSecrettestSecrettestSecrettestSecrettestSecrettestSecrettestSecrettestSecrettestSecret";
+    private static final Long TOKEN_VALIDITY_IN_SECONDS = 1000L;
+
+    @Bean
+    public JwtProperties jwtProperties() {
+        return new JwtProperties(JWT_ISSUER,
+            BASE64_SECRET,
+            TOKEN_VALIDITY_IN_SECONDS);
+    }
+
+}
