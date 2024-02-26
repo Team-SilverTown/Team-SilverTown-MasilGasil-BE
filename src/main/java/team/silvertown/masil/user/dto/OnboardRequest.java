4236b8348839875868ae00db4c78f9ac538dfddc
shoeone96 <dlwnddnjs96@gmail.com>
2024-02-27 00:26:32 +0900

    feat: onboarding 필요한 request 작성

--- src/main/java/team/silvertown/masil/user/dto/OnboardRequest.java
+++ src/main/java/team/silvertown/masil/user/dto/OnboardRequest.java
@@ -1,13 +1,12 @@
 package team.silvertown.masil.user.dto;
 
-import java.util.Date;
 import team.silvertown.masil.user.exception.UserErrorCode;
 import team.silvertown.masil.user.exception.UserValidator;
 
-public record UpdateInfoRequest(
+public record OnboardRequest(
     String nickname,
     String sex,
-    Date birthDate,
+    String birthDate,
     Integer height,
     Integer weight,
     String exerciseIntensity,
@@ -17,16 +16,14 @@ public record UpdateInfoRequest(
     Boolean isUnderAgeConsentConfirmed
 ) {
 
-    public UpdateInfoRequest {
+    public OnboardRequest {
         UserValidator.validateNickname(nickname, UserErrorCode.INVALID_NICKNAME);
         UserValidator.validateSex(sex, UserErrorCode.INVALID_SEX);
         UserValidator.validateBirthDate(birthDate, UserErrorCode.INVALID_BIRTH_DATE);
         UserValidator.validateHeight(height, UserErrorCode.INVALID_HEIGHT);
         UserValidator.validateWeight(weight, UserErrorCode.INVALID_WEIGHT);
-        UserValidator.validateExerciseIntensity(exerciseIntensity,
-            UserErrorCode.INVALID_EXERCISE_INTENSITY);
-        UserValidator.validateIsAllowingMarketing(isAllowingMarketing,
-            UserErrorCode.INVALID_ALLOWING_MARKETING);
+        UserValidator.validateExerciseIntensity(exerciseIntensity, UserErrorCode.INVALID_EXERCISE_INTENSITY);
+        UserValidator.validateIsAllowingMarketing(isAllowingMarketing, UserErrorCode.INVALID_ALLOWING_MARKETING);
         UserValidator.validateIsPersonalInfoConsented(isPersonalInfoConsented, UserErrorCode.INVALID_PERSONAL_INFO_CONSENTED);
         UserValidator.validateIsLocationInfoConsented(isLocationInfoConsented, UserErrorCode.INVALID_LOCATION_INFO_CONSENTED);
         UserValidator.validateIsUnderAgeConsentConfirmed(isUnderAgeConsentConfirmed, UserErrorCode.INVALID_UNDER_AGE_CONSENTED);
