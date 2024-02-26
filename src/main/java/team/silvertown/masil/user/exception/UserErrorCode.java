9377c309edc0b3266cd960dac7dd08963870be55
shoeone96 <dlwnddnjs96@gmail.com>
2024-02-27 00:50:36 +0900

    feat: user nickname 관련 errorcode 추가

---
+++ src/main/java/team/silvertown/masil/user/exception/UserErrorCode.java
@@ -0,0 +1,26 @@
+package team.silvertown.masil.user.exception;
+
+import team.silvertown.masil.common.exception.ErrorCode;
+
+public enum UserErrorCode implements ErrorCode {
+    DUPLICATED_NICKNAME(10020900, "이미 존재하는 닉네임입니다.");
+
+    private final int code;
+    private final String message;
+
+    UserErrorCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public int getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+    
+}
