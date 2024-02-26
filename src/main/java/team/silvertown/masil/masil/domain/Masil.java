0f3b0c8b6120f81ec0cd5057c80692c60ecc8e51
Jay <injaesong0@gmail.com>
2024-02-26 11:16:00 +0900

    feat: 양방향 매핑 설정

--- src/main/java/team/silvertown/masil/masil/domain/Masil.java
+++ src/main/java/team/silvertown/masil/masil/domain/Masil.java
@@ -9,8 +9,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.AccessLevel;
@@ -69,6 +71,9 @@ public class Masil extends BaseEntity {
     @Column(name = "started_at", nullable = false, columnDefinition = "TIMESTAMP(6)")
     @TimeZoneStorage(TimeZoneStorageType.NORMALIZE)
     private OffsetDateTime startedAt;
+
+    @OneToMany(mappedBy = "masil")
+    private List<MasilPin> masilPins = new ArrayList<>();
 
     @Builder
     private Masil(
