45936881439af1c6968ff2e658498feb3e350b46
shoeone96 <dlwnddnjs96@gmail.com>
2024-02-27 00:28:51 +0900

    feat: user 동의항목 저장하는 repository 작성

---
+++ src/main/java/team/silvertown/masil/user/repository/UserAgreementRepository.java
@@ -0,0 +1,8 @@
+package team.silvertown.masil.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.silvertown.masil.user.domain.UserAgreement;
+
+public interface UserAgreementRepository extends JpaRepository<UserAgreement, Long> {
+
+}
