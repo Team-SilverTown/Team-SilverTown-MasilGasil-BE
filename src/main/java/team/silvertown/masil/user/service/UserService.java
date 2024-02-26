21abdfd1e4f29bdf782862bfa9fbb77afacbfd4d
shoeone96 <dlwnddnjs96@gmail.com>
2024-02-27 00:29:13 +0900

    feat: user 추가 정보 작성하는 service 로직 작성

--- src/main/java/team/silvertown/masil/user/service/UserService.java
+++ src/main/java/team/silvertown/masil/user/service/UserService.java
@@ -1,13 +1,17 @@
 package team.silvertown.masil.user.service;
 
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.silvertown.masil.common.exception.DataNotFoundException;
 import team.silvertown.masil.user.domain.User;
-import team.silvertown.masil.user.dto.UpdateInfoRequest;
+import team.silvertown.masil.user.domain.UserAgreement;
+import team.silvertown.masil.user.domain.UserAgreement.UserAgreementBuilder;
+import team.silvertown.masil.user.dto.OnboardRequest;
 import team.silvertown.masil.user.dto.UpdateInfoResponse;
 import team.silvertown.masil.user.exception.UserErrorCode;
+import team.silvertown.masil.user.repository.UserAgreementRepository;
 import team.silvertown.masil.user.repository.UserRepository;
 
 @Service
@@ -15,14 +19,31 @@ import team.silvertown.masil.user.repository.UserRepository;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserAgreementRepository agreementRepository;
 
     @Transactional
-    public UpdateInfoResponse updateInfo(long userId, UpdateInfoRequest request) {
+    public void onboard(long userId, OnboardRequest request) {
         User user = userRepository.findById(userId)
             .orElseThrow(() -> new DataNotFoundException(UserErrorCode.USER_NOT_FOUND));
 
         user.update(request);
-        return null;
+        UserAgreement userAgreement = getUserAgreement(request, user);
+
+        agreementRepository.save(userAgreement);
+    }
+
+    private static UserAgreement getUserAgreement(OnboardRequest request, User user) {
+        LocalDateTime marketingConsentedAt = request.isAllowingMarketing() ? LocalDateTime.now() : null;
+
+        UserAgreement userAgreement = UserAgreement.builder()
+            .user(user)
+            .isAllowingMarketing(request.isAllowingMarketing())
+            .isLocationInfoConsented(request.isLocationInfoConsented())
+            .isPersonalInfoConsented(request.isPersonalInfoConsented())
+            .isUnderAgeConsentConfirmed(request.isUnderAgeConsentConfirmed())
+            .marketingConsentedAt(marketingConsentedAt)
+            .build();
+        return userAgreement;
     }
 
 }
