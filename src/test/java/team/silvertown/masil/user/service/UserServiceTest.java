bfd7ed39d7a495d6e8930e1dc6a4ef8ed9caacc7
shoeone96 <dlwnddnjs96@gmail.com>
2024-02-27 15:41:20 +0900

    refactor: @Nested를 활용하여 테스트 가독성 올리기

--- src/test/java/team/silvertown/masil/user/service/UserServiceTest.java
+++ src/test/java/team/silvertown/masil/user/service/UserServiceTest.java
@@ -1,35 +1,35 @@
 package team.silvertown.masil.user.service;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import team.silvertown.masil.common.exception.DuplicateResourceException;
-import team.silvertown.masil.user.domain.User;
-import team.silvertown.masil.user.exception.UserErrorCode;
-import team.silvertown.masil.user.repository.UserRepository;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import team.silvertown.masil.common.exception.DataNotFoundException;
+import team.silvertown.masil.common.exception.DuplicateResourceException;
 import team.silvertown.masil.config.jwt.JwtTokenProvider;
 import team.silvertown.masil.security.exception.InvalidAuthenticationException;
 import team.silvertown.masil.user.domain.Authority;
 import team.silvertown.masil.user.domain.Provider;
+import team.silvertown.masil.user.domain.User;
 import team.silvertown.masil.user.domain.UserAuthority;
+import team.silvertown.masil.user.exception.UserErrorCode;
 import team.silvertown.masil.user.repository.UserAuthorityRepository;
+import team.silvertown.masil.user.repository.UserRepository;
 
 @AutoConfigureMockMvc
 @DisplayNameGeneration(ReplaceUnderscores.class)
@@ -56,160 +56,168 @@ class UserServiceTest {
     @Mock
     OAuth2User oAuth2User;
 
+    @Nested
+    class 닉네임_중복_조회_로직_테스트 {
 
-    @Test
-    public void 중복닉네임_조회시_존재하지_않는_닉네임을_조회할_경우_정상적으로_통과한다() throws Exception {
-        //given
-        String nickname = faker.name()
-            .fullName();
+        @Test
+        public void 중복닉네임_조회시_존재하지_않는_닉네임을_조회할_경우_정상적으로_통과한다() throws Exception {
+            //given
+            String nickname = faker.name()
+                .fullName();
 
-        //when, then
-        assertDoesNotThrow(() -> userService.checkNickname(nickname));
+            //when, then
+            assertDoesNotThrow(() -> userService.checkNickname(nickname));
+        }
+
+        @Test
+        public void 중복닉네임_조회시_이미_존재하는_닉네임을_조회할_경우_예외가_발생한다() throws Exception {
+            //given, when
+            String nickname = faker.name()
+                .fullName();
+            User user = User.builder()
+                .nickname(nickname)
+                .build();
+            userRepository.save(user);
+
+            //then
+            assertThatThrownBy(() -> userService.checkNickname(nickname))
+                .isInstanceOf(DuplicateResourceException.class)
+                .hasMessage(UserErrorCode.DUPLICATED_NICKNAME.getMessage());
+        }
+
     }
 
-    @Test
-    public void 중복닉네임_조회시_이미_존재하는_닉네임을_조회할_경우_예외가_발생한다() throws Exception {
-        //given, when
-        String nickname = faker.name()
-            .fullName();
-        User user = User.builder()
-            .nickname(nickname)
-            .build();
-        userRepository.save(user);
+    @Nested
+    class 유저_소셜_로그인_회원가입_로직_테스트 {
 
-        //then
-        assertThatThrownBy(() -> userService.checkNickname(nickname))
-            .isInstanceOf(DuplicateResourceException.class)
-            .hasMessage(UserErrorCode.DUPLICATED_NICKNAME.getMessage());
-    }
+        @Test
+        public void 정상적으로_처음_회원가입하는_유저는_유저_정보를_저장한_후_유저객체를_반환한다() throws Exception {
+            //given
+            authorityRepository.deleteAll();
+            userRepository.deleteAll();
 
+            String socialId = String.valueOf(faker.barcode());
+            when(oAuth2User.getName()).thenReturn(socialId);
 
-    @Test
-    public void 정상적으로_처음_회원가입하는_유저는_유저_정보를_저장한_후_유저객체를_반환한다() throws Exception {
-        //given
-        authorityRepository.deleteAll();
-        userRepository.deleteAll();
+            //when
+            User joinedUser = userService.join(oAuth2User, VALID_PROVIDER);
 
-        String socialId = String.valueOf(faker.barcode());
-        when(oAuth2User.getName()).thenReturn(socialId);
+            //then
+            assertThat(joinedUser.getSocialId()).isEqualTo(socialId);
+        }
 
-        //when
-        User joinedUser = userService.join(oAuth2User, VALID_PROVIDER);
+        @Test
+        public void 이미_가입한_유저의_경우_유저정보만_찾은_뒤_반환한다() throws Exception {
+            //given
+            authorityRepository.deleteAll();
+            userRepository.deleteAll();
 
-        //then
-        assertThat(joinedUser.getSocialId()).isEqualTo(socialId);
-    }
+            String socialId = String.valueOf(faker.barcode());
+            when(oAuth2User.getName()).thenReturn(socialId);
 
-    @Test
-    public void 이미_가입한_유저의_경우_유저정보만_찾은_뒤_return한다() throws Exception {
-        //given
-        authorityRepository.deleteAll();
-        userRepository.deleteAll();
+            User user = User.builder()
+                .provider(Provider.get(VALID_PROVIDER))
+                .socialId(socialId)
+                .build();
 
-        String socialId = String.valueOf(faker.barcode());
-        when(oAuth2User.getName()).thenReturn(socialId);
+            userRepository.save(user);
+            List<User> beforeLogin = userRepository.findAll();
 
-        User user = User.builder()
-            .provider(Provider.get(VALID_PROVIDER))
-            .socialId(socialId)
-            .build();
+            //when
+            User joinedUser = userService.join(oAuth2User, VALID_PROVIDER);
+            List<User> afterLogin = userRepository.findAll();
 
-        userRepository.save(user);
-        List<User> beforeLogin = userRepository.findAll();
+            //then
+            assertThat(joinedUser.getSocialId()).isEqualTo(socialId);
+            assertThat(beforeLogin.size()).isEqualTo(afterLogin.size());
+        }
 
-        //when
-        User joinedUser = userService.join(oAuth2User, VALID_PROVIDER);
-        List<User> afterLogin = userRepository.findAll();
+        @Test
+        public void 비정상적인_provider로_회원가입하는_유저는_회원가입에_실패한다() throws Exception {
+            //given, when
+            String socialId = String.valueOf(faker.barcode());
+            when(oAuth2User.getName()).thenReturn(socialId);
 
-        //then
-        assertThat(joinedUser.getSocialId()).isEqualTo(socialId);
-        assertThat(beforeLogin.size()).isEqualTo(afterLogin.size());
-    }
+            //then
+            assertThatThrownBy(() -> userService.join(oAuth2User, INVALID_PROVIDER))
+                .isInstanceOf(InvalidAuthenticationException.class)
+                .hasMessage(UserErrorCode.INVALID_PROVIDER.getMessage());
+        }
 
-    @Test
-    public void 비정상적인_provider로_회원가입하는_유저는_회원가입에_실패한다() throws Exception {
-        //given, when
-        String socialId = String.valueOf(faker.barcode());
-        when(oAuth2User.getName()).thenReturn(socialId);
+        @Test
+        public void OAuth2User_객체가_null로_전달되면_예외가_발생한다() throws Exception {
+            //given, when
+            oAuth2User = null;
 
-        //then
-        assertThatThrownBy(() -> userService.join(oAuth2User, INVALID_PROVIDER))
-            .isInstanceOf(InvalidAuthenticationException.class)
-            .hasMessage(UserErrorCode.INVALID_PROVIDER.getMessage());
-    }
+            //then
+            assertThatThrownBy(() -> userService.join(oAuth2User, INVALID_PROVIDER))
+                .isInstanceOf(InvalidAuthenticationException.class)
+                .hasMessage(UserErrorCode.INVALID_OAUTH2_TOKEN.getMessage());
+        }
 
-    @Test
-    public void OAuth2User_객체가_null로_전달되면_예외가_발생한다() throws Exception {
-        //given, when
-        oAuth2User = null;
+        @Test
+        public void OAuth2User의_name값이_null로_전달되면_예외가_발생한다() throws Exception {
+            //given, when
+            when(oAuth2User.getName()).thenReturn(null);
 
-        //then
-        assertThatThrownBy(() -> userService.join(oAuth2User, INVALID_PROVIDER))
-            .isInstanceOf(InvalidAuthenticationException.class)
-            .hasMessage(UserErrorCode.INVALID_OAUTH2_TOKEN.getMessage());
-    }
+            //then
+            assertThatThrownBy(() -> userService.join(oAuth2User, INVALID_PROVIDER))
+                .isInstanceOf(InvalidAuthenticationException.class)
+                .hasMessage(UserErrorCode.INVALID_OAUTH2_TOKEN.getMessage());
+        }
 
-    @Test
-    public void OAuth2User의_name값이_null로_전달되면_예외가_발생한다() throws Exception {
-        //given, when
-        when(oAuth2User.getName()).thenReturn(null);
+        @ParameterizedTest
+        @NullAndEmptySource
+        @ValueSource(strings = "invalid provider")
+        public void 잘못된_Provider_값이_전달되면_예외가_발생한다(String provider) throws Exception {
+            //given, when,
+            String socialId = String.valueOf(faker.barcode());
+            when(oAuth2User.getName()).thenReturn(socialId);
 
-        //then
-        assertThatThrownBy(() -> userService.join(oAuth2User, INVALID_PROVIDER))
-            .isInstanceOf(InvalidAuthenticationException.class)
-            .hasMessage(UserErrorCode.INVALID_OAUTH2_TOKEN.getMessage());
-    }
+            // then
+            assertThatThrownBy(() -> userService.join(oAuth2User, provider))
+                .isInstanceOf(InvalidAuthenticationException.class)
+                .hasMessage(UserErrorCode.INVALID_PROVIDER.getMessage());
+        }
 
-    @ParameterizedTest
-    @NullAndEmptySource
-    @ValueSource(strings = "invalid provider")
-    public void 잘못된_Provider_값이_전달되면_예외가_발생한다(String provider) throws Exception {
-        //given, when,
-        String socialId = String.valueOf(faker.barcode());
-        when(oAuth2User.getName()).thenReturn(socialId);
+        @Test
+        public void 정상적으로_가입_후_첫_로그인에_성공하는_경우_restrict_권한만_가진다() throws Exception {
+            //given
+            String socialId = String.valueOf(faker.barcode());
+            when(oAuth2User.getName()).thenReturn(socialId);
 
-        // then
-        assertThatThrownBy(() -> userService.join(oAuth2User, provider))
-            .isInstanceOf(InvalidAuthenticationException.class)
-            .hasMessage(UserErrorCode.INVALID_PROVIDER.getMessage());
-    }
+            //when
+            User joinedUser = userService.join(oAuth2User, VALID_PROVIDER);
+            User findUser = userRepository.findById(joinedUser.getId())
+                .get();
+            List<UserAuthority> userAuthorities = authorityRepository.findByUser(findUser);
 
-    @Test
-    public void 정상적으로_가입_후_첫_로그인에_성공하는_경우_restrict_권한만_가진다() throws Exception {
-        //given
-        String socialId = String.valueOf(faker.barcode());
-        when(oAuth2User.getName()).thenReturn(socialId);
+            //then
+            assertThat(userAuthorities).hasSize(1);
+            assertThat(userAuthorities.get(0)
+                .getAuthority()
+                .name()).isEqualTo(Authority.RESTRICTED.name());
+        }
 
-        //when
-        User joinedUser = userService.join(oAuth2User, VALID_PROVIDER);
-        User findUser = userRepository.findById(joinedUser.getId())
-            .get();
-        List<UserAuthority> userAuthorities = authorityRepository.findByUser(findUser);
+        @Test
+        public void 권한이_없는_유저는_로그인_중_권한_예외가_발생한다() throws Exception {
+            //given
+            String socialId = String.valueOf(faker.barcode());
 
-        //then
-        assertThat(userAuthorities).hasSize(1);
-        assertThat(userAuthorities.get(0)
-            .getAuthority()
-            .name()).isEqualTo(Authority.RESTRICTED.name());
-    }
+            User user = User.builder()
+                .provider(Provider.get(VALID_PROVIDER))
+                .socialId(socialId)
+                .build();
 
-    @Test
-    public void 권한이_없는_유저는_로그인_중_권한_예외가_발생한다() throws Exception {
-        //given
-        String socialId = String.valueOf(faker.barcode());
+            User savedUser = userRepository.save(user);
+            String token = tokenProvider.createToken(savedUser.getId());
 
-        User user = User.builder()
-            .provider(Provider.get(VALID_PROVIDER))
-            .socialId(socialId)
-            .build();
+            //when, then
+            assertThatThrownBy(() -> userService.login(token, savedUser))
+                .isInstanceOf(DataNotFoundException.class)
+                .hasMessage(UserErrorCode.AUTHORITY_NOT_FOUND.getMessage());
+        }
 
-        User savedUser = userRepository.save(user);
-        String token = tokenProvider.createToken(savedUser.getId());
-
-        //when, then
-        assertThatThrownBy(() -> userService.login(token, savedUser))
-            .isInstanceOf(DataNotFoundException.class)
-            .hasMessage(UserErrorCode.AUTHORITY_NOT_FOUND.getMessage());
     }
 
 }
