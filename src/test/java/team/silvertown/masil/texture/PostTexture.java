0be973712d74447a4d9ba51f0ffc40162c7cc8a3
Jay <injaesong0@gmail.com>
2024-03-02 03:13:31 +0900

    test: 포스트/포스트핀 텍스쳐 추가

--- src/test/java/team/silvertown/masil/texture/PostTexture.java
+++ src/test/java/team/silvertown/masil/texture/PostTexture.java
@@ -1,9 +1,13 @@
 package team.silvertown.masil.texture;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.locationtech.jts.geom.LineString;
+import org.locationtech.jts.geom.Point;
 import team.silvertown.masil.post.domain.Post;
+import team.silvertown.masil.post.domain.PostPin;
 import team.silvertown.masil.user.domain.User;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -15,6 +19,18 @@ public final class PostTexture extends BaseDomainTexture {
         String thumbnailUrl = MasilTexture.createUrl();
 
         return createPostWithOptional(content, thumbnailUrl);
+    }
+
+    public static Post createDependentPost(User user, int pathSize) {
+        String addressDepth1 = MasilTexture.createAddressDepth1();
+        String addressDepth2 = MasilTexture.createAddressDepth2();
+        String addressDepth3 = MasilTexture.createAddressDepth3();
+        LineString path = MapTexture.createLineString(pathSize);
+        String title = getRandomSentenceWithMax(29);
+        int totalTime = getRandomInt(600, 4200);
+
+        return createPost(user, addressDepth1, addressDepth2, addressDepth3, "", path, title, null,
+            null, (int) path.getLength(), totalTime);
     }
 
     public static Post createPostWithOptional(String content, String thumbnailUrl) {
@@ -56,6 +72,43 @@ public final class PostTexture extends BaseDomainTexture {
             .distance(distance)
             .totalTime(totalTime)
             .build();
+    }
+
+    public static PostPin createPostPin(Post post, Long userId) {
+        String content = faker.harryPotter()
+            .quote();
+        String url = MasilTexture.createUrl();
+        Point point = MapTexture.createPoint();
+
+        return createPostPin(post, point, userId, content, url);
+    }
+
+    public static PostPin createPostPin(
+        Post post,
+        Point point,
+        Long userId,
+        String content,
+        String thumbnailUrl
+    ) {
+        return PostPin.builder()
+            .post(post)
+            .point(point)
+            .userId(userId)
+            .content(content)
+            .thumbnailUrl(thumbnailUrl)
+            .build();
+    }
+
+    public static List<PostPin> createDependentPostPins(Post post, Long userId, int size) {
+        List<PostPin> postPins = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            PostPin postPin = createPostPin(post, userId);
+
+            postPins.add(postPin);
+        }
+
+        return postPins;
     }
 
 }
