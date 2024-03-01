da94f11e6c883af92e094cc04f523f62bfd0e2c2
Jay <injaesong0@gmail.com>
2024-03-01 20:46:25 +0900

    refactor: 마실 생성 로직 단순화

--- src/main/java/team/silvertown/masil/masil/service/MasilService.java
+++ src/main/java/team/silvertown/masil/masil/service/MasilService.java
@@ -48,11 +48,10 @@ public class MasilService {
         User user = userRepository.findById(userId)
             .orElseThrow(getNotFoundException(MasilErrorCode.USER_NOT_FOUND));
         Masil masil = createMasil(request, user);
-        Masil savedMasil = masilRepository.save(masil);
 
-        savePins(request.pins(), savedMasil);
+        savePins(request.pins(), masil);
 
-        return new CreateResponse(savedMasil.getId());
+        return new CreateResponse(masil.getId());
     }
 
     @Transactional(readOnly = true)
@@ -100,7 +99,7 @@ public class MasilService {
     }
 
     private Masil createMasil(CreateRequest request, User user) {
-        return Masil.builder()
+        Masil masil = Masil.builder()
             .depth1(request.depth1())
             .depth2(request.depth2())
             .depth3(request.depth3())
@@ -115,6 +114,8 @@ public class MasilService {
             .totalTime(request.totalTime())
             .user(user)
             .build();
+
+        return masilRepository.save(masil);
     }
 
     private void savePins(List<CreatePinRequest> pins, Masil masil) {
