a70e47d86d2d89820de3f698537526f361ae2695
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-03-01 17:26:07 +0900

    refactor: 정확한 validation을 위해 dateformatter 수정

--- src/main/java/team/silvertown/masil/common/validator/DateValidator.java
+++ src/main/java/team/silvertown/masil/common/validator/DateValidator.java
@@ -7,9 +7,18 @@ import team.silvertown.masil.common.exception.BadRequestException;
 import team.silvertown.masil.user.exception.UserErrorCode;
 
 public class DateValidator {
+
+    private static final String DATE_FORMAT = "\\d{4}-\\d{2}-\\d{2}";
+    private static final String DATE_PATTERN = "yyyy-MM-dd";
+
     public static Date parseDate(String date, UserErrorCode errorCode) {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        if (!date.matches(DATE_FORMAT)) {
+            throw new BadRequestException(errorCode);
+        }
+
+        SimpleDateFormat formatter = new SimpleDateFormat(DATE_PATTERN);
         formatter.setLenient(false);
+
         try {
             return formatter.parse(date);
         } catch (ParseException e) {
