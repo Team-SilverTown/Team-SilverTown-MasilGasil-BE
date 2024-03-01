f453ffdcf1668b5ba542b676b13a80845717d23a
Jay <injaesong0@gmail.com>
2024-03-01 15:08:35 +0900

    fix: swagger ui 작동 안하는 버그 수정

--- src/main/java/team/silvertown/masil/config/SwaggerConfig.java
+++ src/main/java/team/silvertown/masil/config/SwaggerConfig.java
@@ -2,15 +2,13 @@ package team.silvertown.masil.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
     info = @Info(
         title = "마실가실 API"
-    ),
-    security = @SecurityRequirement(name = "카카오 로그인")
+    )
 )
 public class SwaggerConfig {
 
