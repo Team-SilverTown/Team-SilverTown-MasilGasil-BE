e6bce9d7980b99948df7c1f25ac44ebcbd5286c5
Jay <injaesong0@gmail.com>
2024-03-01 20:45:33 +0900

    feat: 포스트 생성 api 엔드포인트 추가

---
+++ src/main/java/team/silvertown/masil/post/controller/PostController.java
@@ -0,0 +1,34 @@
+package team.silvertown.masil.post.controller;
+
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import team.silvertown.masil.post.dto.request.CreateRequest;
+import team.silvertown.masil.post.dto.response.CreateResponse;
+import team.silvertown.masil.post.service.PostService;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/api/v1/posts")
+    public ResponseEntity<CreateResponse> create(
+        @AuthenticationPrincipal
+        Long userId,
+        @RequestBody
+        CreateRequest request
+    ) {
+        CreateResponse response = postService.create(userId, request);
+        URI uri = URI.create("/api/v1/posts/" + response.id());
+
+        return ResponseEntity.created(uri)
+            .body(response);
+    }
+
+}
