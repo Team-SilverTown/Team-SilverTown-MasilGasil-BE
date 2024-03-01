540249edf0bd8424e126a29cee61dc8fcf12e2b4
Jay <injaesong0@gmail.com>
2024-03-01 14:48:41 +0900

    fix: security scheme 제거

--- src/main/java/team/silvertown/masil/config/SwaggerConfig.java
+++ src/main/java/team/silvertown/masil/config/SwaggerConfig.java
@@ -1,12 +1,8 @@
 package team.silvertown.masil.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.OAuthFlow;
-import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -15,16 +11,6 @@ import org.springframework.context.annotation.Configuration;
         title = "마실가실 API"
     ),
     security = @SecurityRequirement(name = "카카오 로그인")
-)
-@SecurityScheme(
-    name = "카카오 로그인",
-    type = SecuritySchemeType.OAUTH2,
-    flows = @OAuthFlows(
-        authorizationCode = @OAuthFlow(
-            authorizationUrl = "${spring.security.oauth2.client.provider.kakao.authorization-uri}",
-            tokenUrl = "${spring.security.oauth2.client.provider.kakao.token-uri}"
-        )
-    )
 )
 public class SwaggerConfig {
 
