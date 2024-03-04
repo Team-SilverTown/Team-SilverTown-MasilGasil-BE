a64b7281d7ee8a27f01a9d37cd89b38ad679751c
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-03-04 20:35:30 +0900

    refactor: DateValidator 를 Validator 상속으로 만들기

--- src/main/java/team/silvertown/masil/common/validator/DateValidator.java
+++ src/main/java/team/silvertown/masil/common/validator/DateValidator.java
@@ -4,17 +4,16 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import team.silvertown.masil.common.exception.BadRequestException;
+import team.silvertown.masil.common.exception.ErrorCode;
 import team.silvertown.masil.user.exception.UserErrorCode;
 
-public class DateValidator {
+public class DateValidator extends Validator{
 
     private static final String DATE_FORMAT = "\\d{4}-\\d{2}-\\d{2}";
     private static final String DATE_PATTERN = "yyyy-MM-dd";
 
-    public static LocalDate parseDate(String date, UserErrorCode errorCode) {
-        if (!date.matches(DATE_FORMAT)) {
-            throw new BadRequestException(errorCode);
-        }
+    public static LocalDate parseDate(String date, ErrorCode errorCode) {
+        notMatching(date, DATE_FORMAT, errorCode);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
 
