31e9d40a52dcd1a3cfae523d54de259e039e340e
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-03-04 17:10:02 +0900

    feat: 내 정보 불러오는 api 작성

--- src/main/java/team/silvertown/masil/user/controller/UserController.java
+++ src/main/java/team/silvertown/masil/user/controller/UserController.java
@@ -2,9 +2,11 @@ package team.silvertown.masil.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import team.silvertown.masil.user.dto.MeInfoResponse;
 import team.silvertown.masil.user.service.UserService;
 
 @RestController
@@ -17,6 +19,11 @@ public class UserController {
     public ResponseEntity<Void> nicknameCheck(@RequestParam String nickname){
         userService.checkNickname(nickname);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/api/v1/users/me")
+    public ResponseEntity<MeInfoResponse> getMyInfo(@AuthenticationPrincipal Long memberId){
+        return ResponseEntity.ok(userService.getMe(memberId));
     }
 
 }
