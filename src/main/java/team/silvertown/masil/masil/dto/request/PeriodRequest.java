ca221e1cc69b165daed7eb4d4cd171a6f0d65766
Jay <injaesong0@gmail.com>
2024-03-06 07:56:30 +0900

    style: 투두 추가

--- src/main/java/team/silvertown/masil/masil/dto/request/PeriodRequest.java
+++ src/main/java/team/silvertown/masil/masil/dto/request/PeriodRequest.java
@@ -3,6 +3,7 @@ package team.silvertown.masil.masil.dto.request;
 import java.time.LocalDate;
 
 public record PeriodRequest(
+    // TODO: apply date parser
     LocalDate startDate,
     LocalDate endDate
 ) {
