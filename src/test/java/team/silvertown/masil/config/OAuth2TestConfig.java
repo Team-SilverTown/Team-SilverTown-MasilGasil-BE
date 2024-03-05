4d02b5427e3233bc85f766ca3ee3ec0bd84f544d
Jay <injaesong0@gmail.com>
2024-03-06 08:34:48 +0900

    rename: 체크스타일 해결

--- src/test/java/team/silvertown/masil/config/OAuth2TestConfig.java
+++ src/test/java/team/silvertown/masil/config/OAuth2TestConfig.java
@@ -1,4 +1,4 @@
-package team.silvertown.masil.testConfig;
+package team.silvertown.masil.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
