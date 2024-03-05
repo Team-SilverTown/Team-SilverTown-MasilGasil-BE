7128f64169c9a45801362b047f3690bbcbb86966
Jay <injaesong0@gmail.com>
2024-03-06 07:51:55 +0900

    style: 상수명 수정

--- src/main/java/team/silvertown/masil/common/util/DateTimeConverter.java
+++ src/main/java/team/silvertown/masil/common/util/DateTimeConverter.java
@@ -11,14 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DateTimeConverter {
 
-    private static final ZoneOffset KrOffset = ZoneOffset.of("+09:00");
+    private static final ZoneOffset KR_OFFSET = ZoneOffset.of("+09:00");
 
     public static OffsetDateTime toBeginningOfDay(LocalDate date) {
-        return date.atTime(OffsetTime.of(LocalTime.MIDNIGHT, KrOffset));
+        return date.atTime(OffsetTime.of(LocalTime.MIDNIGHT, KR_OFFSET));
     }
 
     public static OffsetDateTime toEndOfDay(LocalDate date) {
-        return date.atTime(OffsetTime.of(LocalTime.MAX, KrOffset));
+        return date.atTime(OffsetTime.of(LocalTime.MAX, KR_OFFSET));
     }
 
 }
