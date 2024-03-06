51fcaada1e7aed40eaea10a3e9217b370017f0a1
Jay <injaesong0@gmail.com>
2024-03-06 11:12:07 +0900

    test: 혹시나 하는 CI 예외 방지 수정

--- src/test/java/team/silvertown/masil/mate/domain/MateTest.java
+++ src/test/java/team/silvertown/masil/mate/domain/MateTest.java
@@ -54,7 +54,7 @@ class MateTest {
         gatheringPlacePoint = MapTexture.createPoint();
         gatheringPlaceDetail = MateTexture.getRandomSentenceWithMax(50);
         gatherAt = MateTexture.getFutureDateTime();
-        capacity = MateTexture.getRandomInt(1, 10);
+        capacity = MateTexture.getRandomInt(1, 9);
     }
 
     @Test
