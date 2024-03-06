81d0638092484e85fcf35c3fdd870703975c4e57
shoeone96 <dlwnddnjs96@gmail.com>
2024-03-07 01:07:53 +0900

    refactor: oauth2 의존성 제거

--- src/main/java/team/silvertown/masil/config/security/WebSecurityConfig.java
+++ src/main/java/team/silvertown/masil/config/security/WebSecurityConfig.java
@@ -1,8 +1,5 @@
 package team.silvertown.masil.config.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.HashSet;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,26 +7,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import team.silvertown.masil.config.jwt.JwtAuthenticationFilter;
-import team.silvertown.masil.config.jwt.JwtTokenProvider;
-import team.silvertown.masil.config.oauth2.OAuth2AuthenticationSuccessHandler;
-import team.silvertown.masil.user.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private final UserService userService;
-    private final JwtTokenProvider jwtTokenProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final ObjectMapper objectMapper;
     private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
@@ -49,43 +36,9 @@ public class WebSecurityConfig {
             .rememberMe(AbstractHttpConfigurer::disable)
             .logout(AbstractHttpConfigurer::disable)
             .sessionManagement(
-                management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .oauth2Login(oauth2Login -> oauth2Login
-                .userInfoEndpoint(userInfo -> userInfo
-                    .userAuthoritiesMapper(grantedAuthoritiesMapper()))
-                .successHandler(oauth2AuthenticationSuccessHandler()));
+                management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
-
-    private GrantedAuthoritiesMapper grantedAuthoritiesMapper() {
-        return (authorities) -> {
-            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-
-            authorities.forEach((authority) -> {
-                GrantedAuthority mappedAuthority;
-
-                if (authority instanceof OidcUserAuthority userAuthority) {
-                    mappedAuthority = new OidcUserAuthority(
-                        "OIDC_USER", userAuthority.getIdToken(), userAuthority.getUserInfo());
-                } else if (authority instanceof OAuth2UserAuthority userAuthority) {
-                    mappedAuthority = new OAuth2UserAuthority(
-                        "OAUTH2_USER", userAuthority.getAttributes());
-                } else {
-                    mappedAuthority = authority;
-                }
-
-                mappedAuthorities.add(mappedAuthority);
-            });
-
-            return mappedAuthorities;
-        };
-    }
-
-    @Bean
-    public OAuth2AuthenticationSuccessHandler oauth2AuthenticationSuccessHandler() {
-        return new OAuth2AuthenticationSuccessHandler(userService, jwtTokenProvider, objectMapper);
-    }
-
 
 }
 
