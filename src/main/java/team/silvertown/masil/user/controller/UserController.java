2cfba47005f3c03dd13aba0ca7945f6df22d6825
shoeone96 <dlwnddnjs96@gmail.com>
2024-03-07 01:09:58 +0900

    feat: login 을 위한 api 추가

--- src/main/java/team/silvertown/masil/user/controller/UserController.java
+++ src/main/java/team/silvertown/masil/user/controller/UserController.java
@@ -3,8 +3,12 @@ package team.silvertown.masil.user.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import team.silvertown.masil.user.dto.LoginRequest;
+import team.silvertown.masil.user.dto.LoginResponse;
 import team.silvertown.masil.user.service.UserService;
 
 @RestController
@@ -13,11 +17,22 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("api/v1/users/check-nickname")
-    public ResponseEntity<Void> nicknameCheck(@RequestParam String nickname){
+    @PostMapping("/api/v1/users/login")
+    public ResponseEntity<LoginResponse> login(
+        @RequestBody
+        LoginRequest request
+    ) {
+        return ResponseEntity.ok(userService.login(request.accessToken()));
+    }
+
+    @GetMapping("/api/v1/users/check-nickname")
+    public ResponseEntity<Void> nicknameCheck(
+        @RequestParam
+        String nickname
+    ) {
         userService.checkNickname(nickname);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok()
+            .build();
     }
 
 }
-
