22d09ec5b07489f0fea8baa07f7445cc39c58bbf
shoeone96 <dlwnddnjs96@gmail.com>
2024-03-07 01:11:15 +0900

    feat: 로그인 과정에서 데이터 전달을 위해 dto 생성

---
+++ src/main/java/team/silvertown/masil/user/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package team.silvertown.masil.user.dto;
+
+import team.silvertown.masil.user.exception.UserErrorCode;
+import team.silvertown.masil.user.exception.UserValidator;
+
+public record LoginRequest (
+    String accessToken
+){
+
+    public LoginRequest{
+        UserValidator.notBlank(accessToken, UserErrorCode.INVALID_OAUTH2_TOKEN);
+    }
+}
