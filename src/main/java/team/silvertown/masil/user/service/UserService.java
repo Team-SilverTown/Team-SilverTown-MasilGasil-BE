f3168e8ea2a528023258c413c4701081c1e9919e
shoeone96 <dlwnddnjs96@gmail.com>
2024-03-07 19:30:25 +0900

    feat: 로깅 추가

--- src/main/java/team/silvertown/masil/user/service/UserService.java
+++ src/main/java/team/silvertown/masil/user/service/UserService.java
@@ -3,6 +3,7 @@ package team.silvertown.masil.user.service;
 import java.time.OffsetDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
