3e6aacb7ed26c6fb2f08d09071e949f5a813ae76
shoeone96 <dlwnddnjs96@gmail.com>
2024-03-07 18:15:54 +0900

    refactor: 메서드 통합 및 이름 변경

--- src/main/java/team/silvertown/masil/user/service/OAuthService.java
+++ src/main/java/team/silvertown/masil/user/service/OAuthService.java
@@ -1,9 +1,10 @@
 package team.silvertown.masil.user.service;
 
+import java.io.IOException;
 import team.silvertown.masil.user.dto.OAuthResponse;
 
 public interface OAuthService {
 
-    OAuthResponse oauthResponse(String kakaoToken);
+    OAuthResponse getUserInfo(String kakaoToken) throws IOException;
 
 }
