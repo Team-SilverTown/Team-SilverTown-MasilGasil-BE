38dc7d9c08b4fc36b01d2a158101900c667fbadf
Jay <injaesong0@gmail.com>
2024-03-10 21:17:53 +0900

    fix: date time 응답 형식 잘못나오는 버그 수정

--- src/main/java/team/silvertown/masil/config/WebMvcConfig.java
+++ src/main/java/team/silvertown/masil/config/WebMvcConfig.java
@@ -2,11 +2,9 @@ package team.silvertown.masil.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
