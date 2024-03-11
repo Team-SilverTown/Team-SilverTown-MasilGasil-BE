71bd8ab1d389a6bf4ae4df9b767a823f9850a4d0
ASak1104 <khw56184@gmail.com>
2024-03-11 19:07:16 +0900

    fix: 필요없는 annotation 제거

--- src/main/java/team/silvertown/masil/config/WebMvcConfig.java
+++ src/main/java/team/silvertown/masil/config/WebMvcConfig.java
@@ -3,11 +3,9 @@ package team.silvertown.masil.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
