578467588a564341decd783d64c04da1c128a53a
shoeone96 <dlwnddnjs96@gmail.com>
2024-03-13 00:35:20 +0900

    refactor: errorcode 변경

--- src/main/java/team/silvertown/masil/image/exception/ImageErrorCode.java
+++ src/main/java/team/silvertown/masil/image/exception/ImageErrorCode.java
@@ -8,7 +8,7 @@ import team.silvertown.masil.common.exception.ErrorCode;
 @AllArgsConstructor
 public enum ImageErrorCode implements ErrorCode {
     NOT_SUPPORTED_CONTENT(500_16000, "지원하지 않는 파일 형식 입니다"),
-    FILE_IS_EMPTY(500_16000, "존재하지 않는 이미지 파일입니다.");
+    FILE_IS_EMPTY(500_16001, "비어있는 파일입니다");
 
     private final int code;
     private final String message;
