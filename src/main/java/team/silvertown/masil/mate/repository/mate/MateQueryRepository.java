a6e0f26114e1ba196b79889cd4027fd8ee9b5f22
Jay <injaesong0@gmail.com>
2024-03-14 03:34:03 +0900

    feat: 메이트 목록 조회 쿼리 구현

--- src/main/java/team/silvertown/masil/mate/repository/mate/MateQueryRepository.java
+++ src/main/java/team/silvertown/masil/mate/repository/mate/MateQueryRepository.java
@@ -1,10 +1,19 @@
 package team.silvertown.masil.mate.repository.mate;
 
+import java.util.List;
 import java.util.Optional;
+import team.silvertown.masil.common.scroll.dto.NormalListRequest;
+import team.silvertown.masil.common.scroll.dto.ScrollRequest;
 import team.silvertown.masil.mate.domain.Mate;
+import team.silvertown.masil.mate.dto.MateCursorDto;
+import team.silvertown.masil.post.domain.Post;
 
 public interface MateQueryRepository {
 
     Optional<Mate> findDetailById(Long id);
+
+    List<MateCursorDto> findScrollByAddress(NormalListRequest request);
+
+    List<MateCursorDto> findScrollByPost(Post post, ScrollRequest request);
 
 }
