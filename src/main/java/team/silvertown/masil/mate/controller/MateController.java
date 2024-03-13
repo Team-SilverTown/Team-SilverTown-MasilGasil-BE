f5f7288e37e8d1e65135f2e0f0580156035ab79a
Jay <injaesong0@gmail.com>
2024-03-13 14:12:49 +0900

    feat: 메이트 상세조회 api 엔드포인트 추가

--- src/main/java/team/silvertown/masil/mate/controller/MateController.java
+++ src/main/java/team/silvertown/masil/mate/controller/MateController.java
@@ -5,16 +5,20 @@ import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import team.silvertown.masil.mate.dto.request.CreateMateRequest;
 import team.silvertown.masil.mate.dto.response.CreateMateResponse;
+import team.silvertown.masil.mate.dto.response.MateDetailResponse;
 import team.silvertown.masil.mate.service.MateService;
 
 @RestController
@@ -48,6 +52,25 @@ public class MateController {
 
         return ResponseEntity.created(uri)
             .body(response);
+    }
+
+    @GetMapping("/api/v1/mates/{id}")
+    @Operation(summary = "메이트 상세 조회")
+    @ApiResponse(
+        responseCode = "200",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(implementation = MateDetailResponse.class)
+        )
+    )
+    @SecurityRequirements
+    public ResponseEntity<MateDetailResponse> getDetailById(
+        @PathVariable
+        Long id
+    ) {
+        MateDetailResponse response = mateService.getDetailById(id);
+
+        return ResponseEntity.ok(response);
     }
 
 }
