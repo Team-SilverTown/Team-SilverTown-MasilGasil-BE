a44d79160dc39cb7125b64a66928b50f96373b9e
Jay <injaesong0@gmail.com>
2024-03-13 23:13:17 +0900

    refactor: 커서에 0 허용 및 좋아요 커서 11자리 검증

--- src/main/java/team/silvertown/masil/common/validator/ScrollValidator.java
+++ src/main/java/team/silvertown/masil/common/validator/ScrollValidator.java
@@ -3,6 +3,7 @@ package team.silvertown.masil.common.validator;
 import io.micrometer.common.util.StringUtils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import team.silvertown.masil.common.exception.BadRequestException;
 import team.silvertown.masil.common.scroll.OrderType;
 import team.silvertown.masil.common.scroll.dto.ScrollErrorCode;
 
@@ -10,14 +11,16 @@ import team.silvertown.masil.common.scroll.dto.ScrollErrorCode;
 public final class ScrollValidator extends Validator {
 
     private static final int ID_CURSOR_LENGTH = 11;
+    private static final String INITIAL_CURSUR = "0";
 
     public static void validateCursorFormat(String cursor, OrderType order) {
-        if (StringUtils.isBlank(cursor)) {
+        if (StringUtils.isBlank(cursor) || cursor.equals(INITIAL_CURSUR)) {
             return;
         }
 
         if (OrderType.isMostPopular(order)) {
-            notUnder(cursor.length(), ID_CURSOR_LENGTH, ScrollErrorCode.INVALID_CURSOR_FORMAT);
+            throwIf(cursor.length() != ID_CURSOR_LENGTH,
+                () -> new BadRequestException(ScrollErrorCode.INVALID_CURSOR_FORMAT));
             return;
         }
 
