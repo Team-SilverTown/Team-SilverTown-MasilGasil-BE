273eaf1be56145a745f369c6e784c2e00ba69acc
Jay <injaesong0@gmail.com>
2024-03-19 10:33:03 +0900

    fix: 메이트 조회 시 dto가 아닌 메이트 조회

--- src/main/java/team/silvertown/masil/mate/dto/response/SimpleMateResponse.java
+++ src/main/java/team/silvertown/masil/mate/dto/response/SimpleMateResponse.java
@@ -1,8 +1,12 @@
 package team.silvertown.masil.mate.dto.response;
 
 import java.time.OffsetDateTime;
+import lombok.Builder;
+import team.silvertown.masil.mate.domain.Mate;
 import team.silvertown.masil.mate.domain.MateStatus;
+import team.silvertown.masil.user.domain.User;
 
+@Builder
 public record SimpleMateResponse(
     Long id,
     String title,
@@ -13,5 +17,20 @@ public record SimpleMateResponse(
     String authorNickname,
     String authorProfileUrl
 ) {
+
+    public static SimpleMateResponse from(Mate mate) {
+        User author = mate.getAuthor();
+
+        return SimpleMateResponse.builder()
+            .id(mate.getId())
+            .title(mate.getTitle())
+            .gatheringAt(mate.getGatheringAt())
+            .status(mate.getStatus())
+            .capacity(mate.getCapacity())
+            .authorId(author.getId())
+            .authorNickname(author.getNickname())
+            .authorProfileUrl(author.getProfileImg())
+            .build();
+    }
 
 }
