8e0f83accaae9609b920d0ff170a61d1a30d6c23
ASak1104 <khw56184@gmail.com>
2024-03-20 09:00:49 +0900

    feat: post 좋아요 api 추가

---
+++ src/main/java/team/silvertown/masil/post/controller/LikeController.java
@@ -0,0 +1,74 @@
+package team.silvertown.masil.post.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.net.URI;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import team.silvertown.masil.post.dto.SaveLikeDto;
+import team.silvertown.masil.post.service.LikeService;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "산책로 좋아요 API")
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @PutMapping(
+        value = "/api/v1/posts/{postId}/likes",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "산책로 포스트 좋아요")
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            description = "좋아요 데이터 변경",
+            content = @Content(schema = @Schema(implementation = SaveLikeDto.class))
+        ),
+        @ApiResponse(
+            responseCode = "201",
+            description = "최초 좋아요 데이터 생성",
+            content = @Content(schema = @Schema(implementation = SaveLikeDto.class))
+        ),
+    })
+    @SecurityRequirements
+    public ResponseEntity<SaveLikeDto> save(
+        @AuthenticationPrincipal
+        Long userId,
+        @PathVariable
+        Long postId,
+        @RequestBody
+        SaveLikeDto request
+    ) {
+        SaveLikeDto response = likeService.save(userId, postId, request);
+
+        return createResponseEntity(response.isCreated(), postId)
+            .body(response);
+    }
+
+    private BodyBuilder createResponseEntity(Boolean isCreated, Long postId) {
+        if (Objects.nonNull(isCreated) && isCreated) {
+            URI uri = URI.create("/api/v1/posts/" + postId);
+
+            return ResponseEntity.created(uri);
+        }
+
+        return ResponseEntity.ok();
+    }
+
+}
