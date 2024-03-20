a035e314bbba64c171e41b234aa5296d46a9e4a8
ASak1104 <khw56184@gmail.com>
2024-03-20 09:00:30 +0900

    feat: post 좋아요 서비스 기능 구현

---
+++ src/main/java/team/silvertown/masil/post/service/LikeService.java
@@ -0,0 +1,57 @@
+package team.silvertown.masil.post.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.silvertown.masil.common.exception.DataNotFoundException;
+import team.silvertown.masil.common.validator.Validator;
+import team.silvertown.masil.post.domain.Post;
+import team.silvertown.masil.post.domain.PostLike;
+import team.silvertown.masil.post.domain.PostLikeId;
+import team.silvertown.masil.post.dto.SaveLikeDto;
+import team.silvertown.masil.post.exception.PostErrorCode;
+import team.silvertown.masil.post.repository.PostLikeRepository;
+import team.silvertown.masil.post.repository.PostRepository;
+import team.silvertown.masil.user.exception.UserErrorCode;
+import team.silvertown.masil.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final PostLikeRepository postLikeRepository;
+
+    @Transactional
+    public SaveLikeDto save(Long userId, Long postId, SaveLikeDto request) {
+        validateUserId(userId);
+
+        Post post = postRepository.findById(postId)
+            .orElseThrow(() -> new DataNotFoundException(PostErrorCode.POST_NOT_FOUND));
+        PostLikeId postLikeId = new PostLikeId(userId, postId);
+        PostLike postLike = postLikeRepository.findById(postLikeId)
+            .orElseGet(() -> new PostLike(postLikeId, request.isLike(), true));
+
+        updatePostLike(post, postLike, request.isLike());
+
+        return new SaveLikeDto(postLike.isLike(), postLike.isCreated());
+    }
+
+    private void validateUserId(Long userId) {
+        boolean notExistUser = !userRepository.existsById(userId);
+
+        Validator.throwIf(notExistUser, () -> new DataNotFoundException(UserErrorCode.USER_NOT_FOUND));
+    }
+
+    private void updatePostLike(Post post, PostLike postLike, boolean newState) {
+        if (postLike.hasChange(newState)) {
+            postLike.setIsLike(newState);
+            postLikeRepository.save(postLike);
+        }
+
+        // TODO: 반정규화 필드를 활용한 성능 개선
+        post.setLikeCount(postLikeRepository.countByPostId(post.getId()));
+    }
+
+}
