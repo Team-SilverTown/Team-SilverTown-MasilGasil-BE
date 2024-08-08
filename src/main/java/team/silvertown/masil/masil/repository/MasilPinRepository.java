c668204d79fe263f1ab9a078ed08e863f6663688
ASak1104 <khw56184@gmail.com>
2024-08-09 00:20:35 +0900

    feat: define masil pin delete query

--- src/main/java/team/silvertown/masil/masil/repository/MasilPinRepository.java
+++ src/main/java/team/silvertown/masil/masil/repository/MasilPinRepository.java
@@ -1,8 +1,15 @@
 package team.silvertown.masil.masil.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import team.silvertown.masil.masil.domain.Masil;
 import team.silvertown.masil.masil.domain.MasilPin;
 
 public interface MasilPinRepository extends JpaRepository<MasilPin, Long> {
+
+    @Modifying
+    @Query("DELETE FROM MasilPin mp WHERE mp.masil = :masil")
+    void deleteByMasil(Masil masil);
 
 }
